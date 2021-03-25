9749f1b9296c850fdf756088ba2e1d815e2e9085
bugmaker2 <qby1061688677@gmail.com>
2021-03-25 17:42:49 +0800

    Add a commit message

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -5,7 +5,6 @@ extends RigidBody2D
 func _on_Timer_timeout():
 	queue_free()
 
-
 func _on_Area2D_body_entered(body):
 	if "Enemy" in body.name:
 		queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-const SPEEDSTEP = 2
+const SPEEDSTEP = 3
 const TURNINGRATE = 2
+const BULLET = preload("res://Player/Bullet.tscn")
 const BULLETSPEED = 2000
 var motion = Vector2(0,0)
 var angle = 0
@@ -16,6 +17,17 @@ func _physics_process(delta):
 		angle+=deg2rad(TURNINGRATE)
 		$Sprite.rotation_degrees-=TURNINGRATE
 	motion = move_and_slide(motion)
+
+
+	if Input.is_action_just_pressed("LMB"):
+		fire()
+		
+func fire():
+	var bullet_instance = BULLET.instance()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = 0
+	bullet_instance.apply_impulse(Vector2(),Vector2(BULLETSPEED,0).rotated(deg2rad($Sprite.rotation_degrees-90)))
+	get_tree().get_root().call_deferred("add_child",bullet_instance)
 
 func killed():
 	get_tree().reload_current_scene()
